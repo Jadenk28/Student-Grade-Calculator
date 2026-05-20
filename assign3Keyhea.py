37fe3d0cc296eea73956b9c568155015358967e5
Jadenk28 <JadenKeyhea@outlook.com>
2026-05-20 15:28:39 -0400

    Add files via upload

---
+++ assign3Keyhea.py
@@ -0,0 +1,121 @@
+grades = [
+87.29, 69.08, 82.52, 55.87, 84.38, 62.44, 99.48, 76.07, 65.05, 58.85, 84.79, 94.41,
+83.69, 88.88, 99.12, 52.48, 95.38, 87.32, 93.73, 67.75,52.57, 65.60, 
+80.76, 55.88, 55.12, 99.20, 74.19, 86.43, 54.94, 79.56,64.75, 94.32, 59.58, 63.15, 
+64.90, 98.86, 96.22, 74.67, 80.31, 80.63, 91.91, 57.10, 85.21, 62.89, 54.80, 68.99, 
+53.62, 96.77, 92.70, 72.34,93.43, 67.16, 95.85, 98.43, 88.14, 52.17, 58.62, 
+86.92, 92.47, 62.40, 93.79, 86.27, 60.16, 98.35, 91.75, 66.63, 55.91, 73.05, 
+82.42, 55.85, 73.14, 93.04, 50.07, 75.69, 62.55, 84.36, 93.40, 89.42, 63.96, 92.36,
+74.17, 91.42, 70.85, 78.26, 93.13, 79.29, 67.80, 74.73, 56.97, 69.51,
+64.48, 87.23, 68.67, 82.84, 75.03, 98.31, 89.38, 61.21, 55.69, 60.84,
+69.82, 56.33, 79.93, 79.46, 65.63, 87.39, 50.90, 70.90, 68.38, 69.89,
+93.70, 92.65, 54.20, 74.23, 64.97, 96.44, 70.29, 91.58, 91.05, 60.12,
+82.67, 82.65, 52.18, 57.91, 95.65, 70.13, 93.41, 61.56, 64.72, 59.68,
+80.69, 95.46, 59.98, 65.08, 81.83, 57.26, 91.08, 64.33, 72.32, 62.04,
+61.28, 64.15, 85.30, 87.11, 69.81, 98.76, 71.00, 87.44, 97.24, 64.28,
+53.81, 57.13, 82.43, 52.51, 94.84, 56.66, 70.23, 50.44, 96.66, 50.50,
+58.60, 59.30, 87.18, 84.60, 51.60, 95.48, 91.78, 77.89, 83.49, 83.02,
+55.56, 53.07, 93.13, 57.83, 69.24, 89.31, 90.20, 89.22, 61.96, 76.04,
+94.24, 93.69, 66.70, 87.30, 81.38, 58.81, 73.39, 93.76, 77.76, 66.08,
+94.40, 77.01, 72.50, 52.97, 90.23, 94.77, 75.82, 63.78, 92.71, 90.91,
+77.35, 83.84, 78.83, 99.22, 56.66, 76.07, 53.70, 66.68, 70.27, 71.79,
+74.40, 60.11, 53.10, 63.56, 86.34, 65.38, 77.52, 88.16, 74.27, 95.45,
+61.86, 94.56, 97.74, 94.86, 75.97, 56.84, 86.66, 72.75, 99.35, 89.01,
+99.90, 54.83, 67.55, 66.85, 61.58, 79.11, 72.28, 76.97, 57.96, 92.63,
+76.65, 56.10, 70.37, 61.61, 98.51, 96.53, 75.83, 93.97, 71.46, 93.53
+]
+
+names = [
+"Fayette Slomski", "Lynda Plansinis", "Mechelle Ereaux", "Megen Marashio","Alysia Southwell", "Elisha Gutzler", "Karlene Franssen", "Deloria Tredennick",
+"Charlean Clesca", "Lorilee Jerde", "Rochelle Kaleel", "Becka Taverner","Carolin Bartholomew", "Opalina Thixton", "Keeley Alumbaugh", "Mead Stiller",
+"Mame Acres", "Tisha Grider", "Margaux Safford", "Kelci Blondin","Claudine Mcquigg", "Aurie Spinney", "Nancey Maruscak", "Marlene Topacio",
+"Ronalda Stoyer", "Caria Kuehnert", "Minnaminnie Stflorant", "Regine Latus",
+"Florrie Lovet", "Marcelline Morence", "Amanda Keylor", "Janean Luptak", "Shaun Sandine", "Roseanne Noffsinger", "Beverlie Reimel", "Nelle Vanert",
+"Andeee Schamberger", "Bobbye Sandona", "Talia Sullen", "Sherri Blain",
+"Margarethe Hendrixson", "Gaylene Diop", "Felicity Chirdon", "Peri Fagerquist",
+"Willetta Cheverez", "Licha Paynes", "Samantha Lapek", "Jolee Nasuti",
+"Antonia Stoot", "Kristen Marcantel", "Sadella Aschan", "Anet Ealand",
+"Mariele Guldemond", "Britta Axman", "Ainsley Zedian", "Kimberlyn Bicknese",
+"Marisa Karcich", "Denice Pfeil", "Martita Cutchember", "Pearla Folk",
+"Selene Delgenio", "Steffane Sharlin", "Alyson Morea", "Jenifer Dorough",
+"Livy Therrell", "Maxie Nwakanma", "Rennie Ehly", "Diena Gahlman",
+"Francoise Swetnam", "Bobbee Passarelli", "Darcy Calvaresi", "Winona Mccroy",
+"Cecilla Bruse", "Justinn Hyers", "Shanda Mcdonnall", "Doti Bingley",
+"Tiff Galayda", "Lauryn Dennard", "Neilla Mercurio", "Britni Peloquin",
+"Lilllie Fiori", "Eda Dille", "Julee Heykoop", "Cora Lookabaugh",
+"Emily Townsand", "Josephina Follansbee", "Luella Whitebird", "Kattie Rawhoof",
+"Vinnie Standre", "Kacy Bianco", "Latashia Grover", "Tabbatha Egerer",
+"Donielle Szymanowski", "Robby Farinacci", "Patrice Swider", "Aila Remson",
+"Vinnie Mcgarvie", "Florina Corino", "Melisent Niswander", "Arline Kowal",
+"Ashely Seckletstewa", "Corrina Luce", "Shaylah Mcspedon", "Lorrayne Botero",
+"Ericha Michales", "Shirlee Pylvainen", "Trixie Madine", "Cass Chica",
+"Tonya Laudat", "Gillian Ibarra", "Karolina Flath", "Maegan Scanio",
+"Quentin Overstreet", "Vivianne Paulus", "Adelaide Zalwsky", "Selestina Donota",
+"Ryann Vilanova", "Reeta Harth", "Cindee Stuzman", "Maure Kallmeyer",
+"Zonnya Ewell", "Rosetta Poire", "Catlaina Ledden", "Barbara Barella",
+"Janie Hadesty", "Mia Branz", "Dani Chopp", "Julianne Spirko",
+"Maxie Bertschy", "Karlene Vigne", "Charlean Biermann", "Dari Ganesh",
+"Sidoney Amerman", "Iormina Bawcombe", "Marga Miralles", "Windy Vaux",
+"Willetta Dicus", "Kellyann Uhler", "Kaye Tibbert", "Umeko Hamn",
+"Corri Bingert", "Giana Osisek", "Glynnis Szwaja", "Annabelle Komatsu",
+"Dee Montville", "Glenn Colla", "Lindy Eley", "Gertrudis Nale",
+"Vonni Armintrout", "Olva Shellum", "Sula Freise", "Stefania Seryak",
+"Tallulah Sholar", "Hanny Reaid", "Conni Obermeier", "Morganne Pollet",
+"Virginie Kehler", "Nellie Draheim", "Cassi Menitz", "Mirelle Ekker",
+"Corry Sivic", "Dinnie But", "Phaedra Holubar", "Agace Kinniburgh",
+"Benedicta Auwarter", "Camella Batkin", "Sadie Galvani", "Tarah Perham",
+"Marys Nogle", "Sula Kunkleman", "Verene Jemison", "Lauretta Pultorak",
+"Saudra Elke", "Veronike Matthis", "Sapphire Schramel", "Vivian Baugess",
+"Kissiah Mattison", "Lavinia Renollet", "Lindsey Shifflett", "Philly Goddard",
+"Noami Demille", "Anne Bryla", "Emma Ihenyen", "Camella Ibach",
+"Trix Gualtieri", "Nara Ridgway", "Olwen Vermont", "Danny Brackemyre",
+"Tobe Eull", "Kania Arvizu", "Mellicent Ohland", "Rozalin Lage",
+"Gilberte Kleman", "Aubine Bankey", "Albertine Maulin", "Charmain Latson",
+"Moyna Appana", "Nanette Hardway", "Larine Shofestall", "Alicia Ollison",
+"Dareen Mcquown", "Aveline Homan", "Lorrin Golackson", "Petronilla Yonashiro",
+"Arleta Waas", "Salli Eichelberger", "Leigh Hootman", "Loree Liberman",
+"Celle Dudgeon", "Nicolea Nahmias", "Hally Martelli", "Carly Setaro",
+"Lorry Navas", "Gene Vallieres", "Ester Koeppe", "Anjanette Costner",
+"Rachele Yoshihara", "Rozamond Keeling", "Patrica Fowley", "Dasie Winik",
+"Aubine Krzal", "Rozalie Fiereck", "Flossi Vonasek", "Vanni Kapoi",
+"Marianna Hooser", "Donella Stuessy", "Jehanna Herder", "Deidre Malbaurn",
+"Gertruda Werts", "Iolanthe Seburg", "Nadia Mcinnish", "Tasha Waldrop",
+"Shelbi Patillo", "Robinette Helfert", "Tamarah Kempon", "Tami Menor",
+"Juline Epolito", "Margette Trouser", "Jeni Macera", "Kaylee Geffers",
+"Filide Yannantuono", "Jaclin Marling", "Morissa Vancamp", "Lezlie Youmans",
+"Lucille Trepagnier", "Tracee Mihara", "Lilas Livecchi", "Melesa Tantillo",
+"Callie Moribayed", "Patience Jex"
+]
+total = 0 
+min = 9999
+max = -9999
+avgPlus = 0
+avgMinus = 0
+maxName = ""
+minName = ""
+
+for i in range(len(grades)):
+    total = total +  grades[i]
+    if grades[i] < min:
+        min = grades[i]
+        minName = names[i]
+    if grades[i] > max:
+        max  = grades[i]
+        maxName = names[i]
+
+avg = total/len(grades)
+
+for i in range(len(grades)):
+    if  grades[i] > avg:
+        avgPlus +=1
+    else:
+     avgMinus +=1
+
+avgPlusCount = (avgPlus/len(grades)) * 100
+avgMinusCount = (avgMinus/len(grades)) * 100
+
+print("Average: ",avg)
+print(f"Maxmimum: {max} ({maxName})" )
+print(f"Minimum: {min} ({minName})")
+print(f"Grades above average: {avgPlus}, {avgPlusCount}%")
+print(f"Grades below average: {avgMinus}, {avgMinusCount}%")
